9715419a63546517118f7e8b54c69baa22bea787
thiago-bernuncio <thiago.bernuncio@gmail.com>
2026-09-05 21:34:22 -0400

    feat: adicionado formulário de tarefa controlada

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,25 @@
-
+import {useState} from 'react'
 import './App.css'
 
 function App() {
+  const [isFormOpen, setIsFormOpen] = useState(false)
+  const [title, setTitle] = useState('')
+
+  function handleCloseForm(){
+    setIsFormOpen(false)
+    setTitle('')
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+
+    console.log({
+      title: title,
+    })
+
+    setTitle('')
+    setIsFormOpen(false)
+  }
 
   return (
     <div className="app">
@@ -18,10 +36,53 @@ function App() {
             <p>0 tarefas cadastradas</p>
           </div>
 
-          <button type="button">
+          <button type="button" onClick={() => setIsFormOpen(true)}>
             Nova tarefa
           </button>
         </section>
+
+        {isFormOpen && (
+          <section className="task-form-card">
+            <h2>Criar nova tarefa</h2>
+
+            <form onSubmit={handleSubmit}>
+              <div className='form-group'>
+                <label htmlFor="task-title">
+                  Título
+                </label>
+
+                <input
+                  id="task-title"
+                  type="text"
+                  placeholder="Ex.: Estudar React"
+                  value={title}
+                  onChange={(event) => setTitle(event.target.value)} 
+                  maxLength={60}
+                  required              
+                  /> 
+
+                <span className="character-counter">
+                  {title.length}/60 caracteres
+                </span>
+              </div>
+
+              <div className="form-actions">
+                <button 
+                className="secondary-button"
+                type="button"
+                onClick={handleCloseForm}>
+                  Cancelar
+                </button>
+
+                <button 
+                className="primary-button" 
+                type="submit">
+                  Salvar tarefa
+                </button>
+              </div>
+            </form>
+          </section>
+        )}
 
         <section className="empty-state">
           <h2>Nenhuma tarefa cadastrada</h2>
