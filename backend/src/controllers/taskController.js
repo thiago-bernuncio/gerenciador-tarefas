00057f7f37fd1a376d8bf1aa43cd579dc1330caa
thiago-bernuncio <thiago.bernuncio@gmail.com>
2026-09-20 20:15:33 -0400

    feat: Adicionado endpoint de criação de tarefa

--- backend/src/controllers/taskController.js
+++ backend/src/controllers/taskController.js
@@ -1,8 +1,56 @@
+import {randomUUID} from 'node:crypto'
 import {tasks} from '../data/tasks.js'
 
 export function listTasks(request, response) {
     return response.status(200).json({
         data: tasks,
         total: tasks.length,
+    })
+}
+
+export function createTask(request, response){
+    const {
+        title,
+        priority = 'medium',
+        dueDate = null,
+    } = request.body
+
+    const normalizedTitle = 
+        typeof title === 'string'
+            ? title.trim()
+            : ''
+    
+    if(!normalizedTitle) {
+        return response.status(400).json({
+            error: 'O título é obrigatório'
+        })
+    }
+
+    if(normalizedTitle.length > 60) {
+        return response.status(400).json({
+            error: 'O título deve ter no máximo 60 caracteres'
+        })
+    }
+
+    const validPriorities = [
+        'low',
+        'medium',
+        'high',
+    ]
+
+    const newTask = {
+        id: randomUUID(),
+        title: normalizedTitle,
+        priority,
+        dueDate: dueDate || null,
+        completed: false,
+        createdAt: new Date().toISOString(),
+        updatedAt: null,
+    }
+
+    tasks.push(newTask)
+
+    return response.status(201).json({
+        data: newTask
     })
 }
--- backend/src/routes/taskRoutes.js
+++ backend/src/routes/taskRoutes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { listTasks } from "../controllers/taskController.js";
+import { createTask, listTasks } from "../controllers/taskController.js";
 
 const taskRoutes = Router()
 
 taskRoutes.get('/', listTasks)
+
+taskRoutes.post('/', createTask)
 
 export default taskRoutes
